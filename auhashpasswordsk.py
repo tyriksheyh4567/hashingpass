2a84a8a4a1c5f5ab793f7bb8899e9914cdf614a9
Yaromir Sheydaev <148737776+tyriksheyh4567@users.noreply.github.com>
2025-01-06 22:27:46 +0700

    Update auhashpasswordsk.py

--- auhashpasswordsk.py
+++ auhashpasswordsk.py
@@ -1,8 +1,11 @@
+#Author: yaros/tyriksheyh4567/Yaromir Sheydaev.
+#This is public domain, everything about things you can do with the code is in the LICENSE file.
+#Ver 1.0. Made in 06.01.2025.
 import hashlib #library to hash
 import maskpass as mp #library to hide the password
 def hash_password(password): #function to hash the password
     return hashlib.sha512(password.encode()).hexdigest()
-
+#WARNING! You should change the value of "password" variable to hashed one (instruction below)
 password = 'your_hashed_password' #use the hash_password function to hash and print the password, then enter the hashed result
 
 input_password = mp.advpass("amogus! password: ") #input password, but hidden with asterisks
