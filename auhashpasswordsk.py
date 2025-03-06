0701b8f3b0d30f79490c3d7baf43c76ab84e8342
Yaromir Sheydaev <148737776+tyriksheyh4567@users.noreply.github.com>
2025-03-06 11:05:25 +0700

    Update auhashpasswordsk.py

--- auhashpasswordsk.py
+++ auhashpasswordsk.py
@@ -1,6 +1,6 @@
-#Orignal author: yaros/tyriksheyh4567/Yaromir Sheydaev. No need for credit if you fork it (even if your code will be closed source).
+#Orignal author: yarosheyh/tyriksheyh4567/Yaromir Sheydaev. No need for credit if you fork it (even if your code will be closed source).
 #This is public domain, everything about things you can do with the code is in the LICENSE file.
-#Ver 1.1. Made in 04.02.2025. Updated the comments.
+#Ver 1.2. Made in 06.03.2025. Updated the comments.
 import hashlib #library to hash
 import maskpass as mp #library to hide the password
 def hash_password(password): #function to hash the password
